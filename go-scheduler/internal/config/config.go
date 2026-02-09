8cfd34cba0b48c61ab1ab8c1dd2b97b224308405
Bimos6 <bimos6@mail.com>
2026-02-10 01:31:01 +0300

    feat/initial config

---
+++ go-scheduler/internal/config/config.go
@@ -0,0 +1,16 @@
+package config
+
+import "time"
+
+type Config struct {
+	LaravelURL  string        `mapstructure:"LARAVEL_URL"`
+    ServiceKey  string        `mapstructure:"SERVICE_KEY"`
+    
+    RedisURL    string        `mapstructure:"REDIS_URL"`
+    QueueName   string        `mapstructure:"QUEUE_NAME"` 
+    
+    Interval    time.Duration `mapstructure:"INTERVAL_SECONDS"`
+    Timeout     time.Duration `mapstructure:"TIMEOUT_SECONDS"` 
+    
+    LogLevel    string        `mapstructure:"LOG_LEVEL"` 
+}
