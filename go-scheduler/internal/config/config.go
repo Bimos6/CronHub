d6cd363c25bfb989a1831d6906f39b20b6129019
Bimos6 <bimos6@mail.com>
2026-02-10 01:48:42 +0300

    fix/purge

--- go-scheduler/internal/config/config.go
+++ go-scheduler/internal/config/config.go
@@ -3,14 +3,14 @@ package config
 import "time"
 
 type Config struct {
-	LaravelURL  string        `mapstructure:"LARAVEL_URL"`
+    LaravelURL  string        `mapstructure:"LARAVEL_URL"`
     ServiceKey  string        `mapstructure:"SERVICE_KEY"`
     
     RedisURL    string        `mapstructure:"REDIS_URL"`
-    QueueName   string        `mapstructure:"QUEUE_NAME"` 
+    QueueName   string        `mapstructure:"QUEUE_NAME"`
     
     Interval    time.Duration `mapstructure:"INTERVAL_SECONDS"`
-    Timeout     time.Duration `mapstructure:"TIMEOUT_SECONDS"` 
+    Timeout     time.Duration `mapstructure:"TIMEOUT_SECONDS"`
     
-    LogLevel    string        `mapstructure:"LOG_LEVEL"` 
+    LogLevel    string        `mapstructure:"LOG_LEVEL"`
 }
